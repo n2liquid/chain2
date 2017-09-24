6028db7ec5d0c1e3f09c3924d3bda63d38c5a500
Guilherme Vieira <super.driver.512@gmail.com>
2017-09-24 05:10:52 -0300

    WIP.

--- demo/levels/fyrya.js
+++ demo/levels/fyrya.js
@@ -27,36 +27,36 @@ module.exports = st => runFlow({
 
   EV1500: async () => {
     await ln(`<sdl:30>Welcome to Fyrya,<sec:0.4> the most popular dungeon city of the`);
-    await ln(`Kingdom of Yggdrasil!<sec:1.5>`);
+    await ln(`Kingdom of Yggdrasil!<w>`);
     await ln();
 
     await ln(`The city sits on a common travel and commerce path`);
     await ln(`that connects the capital,<sec:0.2> Freunheira,<sec:0.2> to the Old`);
-    await ln(`World outside our Queen's Great Holy Land.<sec:1.5>`);
+    await ln(`World outside our Queen's Great Holy Land.<w>`);
     await ln();
 
     await ln(`It's a rich city that thrives on a dangerous but`);
     await ln(`profitable dungeon.<sec:0.5> Lots of adventurers come by`);
     await ln(`the city to explore its dungeons,<sec:0.2> including many`);
-    await ln(`novices who stay at the upper levels.<sec:1.5>`);
+    await ln(`novices who stay at the upper levels.<w>`);
     await ln();
 
     await ln(`Oh.<sec:0.5> You do look severely underequipped!<sec:0.5> Is that`);
-    await ln(`precisely what you're here for?<sec:1.5>`);
+    await ln(`precisely what you're here for?<w>`);
     await ln();
 
     await ln(`You'll have to register at the Adventure Registration`);
-    await ln(`Office before you're allowed into the dungeon.<sec:2>`);
+    await ln(`Office before you're allowed into the dungeon.<w>`);
     await ln();
 
     await ln(`... Don't gimme that look!<sec:0.8> They'll be offering you free`);
     await ln(`automatic post-death time-traveling under the Queen's`);
     await ln(`magical Grace.<sec:0.8> Judging by your rusty sword,<sec:0.2> you'll be`);
-    await ln(`using the service a lot!<sec:1.5>`);
+    await ln(`using the service a lot!<w>`);
     await ln();
 
     await ln(`Well,<sec:0.3> feel free to explore the city first,<sec:0.3> traveler!<sec:0.8>`);
-    await ln(`Do make yourself home!<sec:1.5>`);
+    await ln(`Do make yourself home!<w>`);
     await ln();
 
     await hr();
